f353679a09c1ba07e998fee6d849aaa3aa2d772a
moses <luckymoses7777@gmail.com>
2024-01-03 12:18:23 +0530

    moses

---
+++ networking tasks/active_interfaces.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Active Network Interfaces:"
+
+# Using ifconfig command
+echo -e "\nUsing ifconfig command:"
+ifconfig -a
+
+# Using ip command
+echo -e "\nUsing ip command:"
+ip addr show
+
+# Using netstat command
+echo -e "\nUsing netstat command:"
+netstat -i
+
+# Additional details using ethtool (install if not available)
+if command -v ethtool &> /dev/null; then
+    echo -e "\nAdditional details using ethtool:"
+    for interface in $(ifconfig -a | grep '^[a-zA-Z]' | awk '{print $1}'); do
+        echo -e "\nInterface: $interface"
+        ethtool $interface
+    done
+
